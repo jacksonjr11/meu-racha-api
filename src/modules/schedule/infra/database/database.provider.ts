44f4b5a7bb435d39468e3ae40f3e9a3ff68cfc8f
Jackson Augusto <jackson.augusto.1571@gmail.com>
2024-10-09 19:20:16 -0300

    Structed folders to apply modules

---
+++ src/modules/schedule/infra/database/database.provider.ts
@@ -0,0 +1,30 @@
+import { Global, Module } from '@nestjs/common';
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: DataSource,
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        username: process.env.DB_USER,
+        host: process.env.DB_SERVER,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: Number(process.env.DB_PORT),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
+
+@Global()
+@Module({
+  providers: [...databaseProviders],
+  exports: [...databaseProviders],
+})
+export class DbModule {}
